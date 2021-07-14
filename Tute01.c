a9b7ac75f802dd380c13ba99236900844666078d
IT21055058 <87405524+IT21055058@users.noreply.github.com>
2021-07-14 03:47:07 +0000

    Exercise 1

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,14 @@
 #include <stdio.h>
 
 int main() {
+  int m1, m2;
+
+  printf("Enter marks for subject 1- ");
+  scanf("%d", &m1);
+  printf("Enter marks for subject 2- ");
+  scanf("%d", &m2);
+
+  printf("Average is %.2f",(m1+m2)/2.0);
   
   return 0;
 }
